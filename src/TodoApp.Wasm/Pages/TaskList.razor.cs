1b2fa3b0916262212025f00994dcfe8e67effa69
vinh-le <levinhtxbt@gmail.com>
2023-09-26 21:59:28 +0700

    Task Filter Component

--- src/TodoApp.Wasm/Pages/TaskList.razor.cs
+++ src/TodoApp.Wasm/Pages/TaskList.razor.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using TodoApp.Model.Dto;
-using TodoApp.Model.Dto.User;
 using TodoApp.Wasm.Services;
 
 namespace TodoApp.Wasm.Pages;
@@ -10,34 +9,18 @@ public partial class TaskList
     private IEnumerable<TaskDto> tasks;
 
     private SearchTaskDto searchQuery = new();
-    
-    private List<AssigneeDto> assignees = new();
-    
-    
+
     [Inject]
     private ITaskApiServices taskApiServices { get; set; }
-    
-    [Inject]
-    private IUserApiService UserApiService { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
         tasks = await taskApiServices.GetTasksAsync(searchQuery);
-        assignees = await UserApiService.GetAssigneesAsync();
     }
 
-    private async Task Search()
+    private async Task OnSearch(SearchTaskDto searchQuery)
     {
-        tasks = await taskApiServices.GetTasksAsync(searchQuery);
-    }
-
-    private async Task ClearSearch()
-    {
-        searchQuery.Priority = null;
-        searchQuery.Status = null;
-        searchQuery.Title = null;
-        searchQuery.AssigneeId = null;
+        this.searchQuery = searchQuery;
         tasks = await taskApiServices.GetTasksAsync(searchQuery);
     }
 }
-
